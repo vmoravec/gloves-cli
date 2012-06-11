589bb447df0fde8474c0960936ab42891c9d1d7b
Vladimir Moravec <vmoravec@suse.com>
2012-06-11 13:45:02 +0200

    Remove the sample code

--- app/lib/gloves-cli.rb
+++ app/lib/gloves-cli.rb
@@ -16,11 +16,11 @@ rescue LoadError
 end
 
 #puts "GT is a constant for Glove::Timezone. Usage: GT.read 'kind' => 'timezones'"
-GT = Glove::Timezone
-@regions = GT.read 'kind'=>'regions', 'only' => 'Europe'
-@language = GT.read 'kind' => 'language'
-@table = Terminal::Table.new :title => 'Regions'.color('#3465A4'), :rows => @regions.to_a.take(10)
-puts @table
-@table = Terminal::Table.new :title => 'Language'.color(:red), :rows => @language.to_a
-puts @table
+# GT = Glove::Timezone
+# @regions = GT.read 'kind'=>'regions', 'only' => 'Europe'
+# @language = GT.read 'kind' => 'language'
+# @table = Terminal::Table.new :title => 'Regions'.color('#3465A4'), :rows => @regions.to_a.take(10)
+# puts @table
+# @table = Terminal::Table.new :title => 'Language'.color(:red), :rows => @language.to_a
+# puts @table
 
